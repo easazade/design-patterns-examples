9fe50e257839d8ed179ced14db4f58341347a840
alireza <easazade@gmail.com>
2019-03-16 03:33:10 +0330

    commit

--- src/main/kotlin/objectpool/Main.kt
+++ src/main/kotlin/objectpool/Main.kt
@@ -2,8 +2,13 @@ package objectpool
 
 fun main(args: Array<String>) {
     /*
-    each battery creates 1 second to get created so we are using an object pool
-    this way we don't have to use a new battery every single time we need one we use already created ones
+    each battery creates 1 second to get created
+    --------------------------------------------
+    Object pools (otherwise known as resource pools) are used to manage the object caching.
+    A client with access to a Object pool can avoid creating a new Objects by simply asking the pool for
+    one that has already been instantiated instead. Generally the pool will be a growing pool, i.e. the
+    pool itself will create new objects if the pool is empty, or we can have a pool, which restricts
+    the number of objects created.
      */
 
 
